af3f51a4626585cf85993e2db9d0322b24b95354
Ryan Stonebraker <ryanastonebraker@gmail.com>
2017-10-12 20:08:10 -0800

    Working account access features git status git status

--- web/js/sign_on.js
+++ web/js/sign_on.js
@@ -1,5 +1,36 @@
-// $('article').append(\"testTSETSETSE\");
 $(document).ready(function() {
+  var validID = false;
+
+
+  var stored_cookie = Cookies.get('PETDORA_USER_ACCOUNT');
+
+  if (stored_cookie) {
+  stored_cookie = stored_cookie.split(',');
+  validID = jQuery.get('../php/accounts.txt', function(data){
+    var splitInfo = data.split("\n");
+    for (var i = 0; i < splitInfo.length; ++i) {
+      if (stored_cookie[1] == splitInfo[i]) {
+        return true;
+      }
+    }
+    return false;
+  });
+  }
+  if (validID) {
+    $('.signin').remove();
+    $('nav').append('<a class = "signin">' + "Welcome " + stored_cookie[0] + '!</a>');
+    $('.signin').css('color', 'black');
+    $('.signin').css('cursor', 'auto');
+
+    // TODO: REPLACE with more content for final
+    var secretText = "These are secret premium features that can ONLY be accessed \
+    with a purchased account...";
+
+    $('.center-wrap').append("<article><h2>Secret Premium Account Features!</h2>\
+    <div class='article-head'></div>"+secretText+"</article>");
+  }
+
+  else if (!validID) {
   $(".signin").on("click", function() {
     $("body").prepend("\
     <div class = \"login_focus\">\
@@ -22,4 +53,6 @@ $(document).ready(function() {
         $('body').css('overflow','');
     });
   });
+
+  }
 });
--- web/php/login_authenticate.php
+++ web/php/login_authenticate.php
@@ -1,8 +1,23 @@
 <?php
+
+// CAN DELETE, JUST FOR TESTING PURPOSES
+ob_start();
+
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$userPassHash = $username+$password;
+$userPassHash = $username . $password;
 
-setcookie('PETDORA_USER_ACCOUNT', '$userPassHash', time() + 120, '/');
+$allowedusersRAW = file_get_contents('accounts.txt');
+$allowedusers = explode("\n", $allowedusersRAW);
+
+for ($i=0; $i < count($allowedusers); $i++) {
+  if ($userPassHash == $allowedusers[$i]) {
+    echo "Welcome ".$username."!";
+    setcookie('PETDORA_USER_ACCOUNT', $username.",".$userPassHash, time() + 1200, '/');
+  }
+  echo "<br>";
+}
+
+header('Location: '.$_SERVER['HTTP_REFERER']);
 ?>
